a84587ee442361d4f85203013c0c15d72f55a3cb
Velms <78916991+Velms@users.noreply.github.com>
2023-12-01 16:12:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,16 +19,14 @@ class App extends Component {
   addContact = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
 
-    const isAdded = this.state.contacts.find;
-    (el => {
-      if (el.name.toLowerCase() === normalizedName) {
-        alert(`${name} is already in contacts`);
-        isAdded = true;
-      }
-    });
+    const isAdded = this.state.contacts.find;{
+      return (el.name.toLowerCase() === normalizedName) 
+    }
+      
 
     if (isAdded) {
       alert(`${name} is already in contacts`);
+      isAdded = true;
       return;
     }
     const contact = {
